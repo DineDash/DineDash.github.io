d6a358e51233d18e8719cf7ede57e4e0f68b5eb0
JancoSOUS <jancobuitendag@gmail.com>
2022-02-17 00:05:15 +0200

    Update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "f390bec00eb6c96531bfac7498ee58ee",
-"/": "f390bec00eb6c96531bfac7498ee58ee",
+  "index.html": "6f4ee7c1d479b8a5baef60eb45808c0c",
+"/": "6f4ee7c1d479b8a5baef60eb45808c0c",
 "main.dart.js": "370738949be6cd67ba64a165f94c8d6a",
 "assets/AssetManifest.json": "a7300d06fae5e2762efc799dc045cfa5",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
